e3d76b8676f8eccbde346acb83ba3cd96f966128
LapaJr31 <jarlat04@gmail.com>
2023-05-03 23:34:30 +0200

    start of the work

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,28 @@
-#![allow(non_snake_case)]
-fn main() {
-    println!("Hello, world!");
+use std::io::Read;
+use crossterm::terminal;
+
+struct CleanUp;
+
+
+impl Drop for CleanUp {
+    fn drop(&mut self) {
+        terminal::disable_raw_mode()
+            .expect("Could not disable raw mode")
+    }
+}
+
+fn  main() {
+
+    let _clean_up = CleanUp;
+
+    terminal::enable_raw_mode()
+        .expect("Could not turn on Raw mode");
+
+    let mut buf = [0; 1];
+    while std::io::stdin()
+        .read(&mut buf).expect("Failed to read line") == 1 && buf != [b'q'] {}
+
+    panic!();
+
+    //terminal::disable_raw_mode();
 }
