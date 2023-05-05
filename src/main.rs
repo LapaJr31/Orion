a2d16536bc381eeec0530f661e9b4c985230bbaf
LapaJr31 <jarlat04@gmail.com>
2023-05-05 23:30:09 +0200

    some stuff too lazy to list just check what I added

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 //use std::io::Read;
 use crossterm::event::*;
-use crossterm::{terminal, event, execute, cursor};
+use crossterm::{terminal, event, execute, cursor, queue};
 use crossterm::terminal::ClearType;
 use std::io::stdout;
 use std::time::Duration;
+use std::io;
+use std::io::Write;
+
+const VERSION: &str = "0.0.1";
+
 
 struct CleanUp;
 
@@ -19,9 +24,50 @@ impl Drop for CleanUp {
     }
 }
 
+struct EditorContents{
+    content: String,
+}
+
+
+impl EditorContents {
+    fn new() -> Self {
+        Self{
+            content: String::new(),
+        }
+    }
+
+    fn push(&mut self, ch: char) {
+        self.content.push(ch)
+    }
+
+    fn str_push(&mut self, string: &str) {
+        self.content.push_str(string)
+    }
+
+}
+
+impl io::Write for EditorContents {
+    fn write (&mut self, buf: &[u8]) -> io::Result<usize> {
+        match std::str::from_utf8(buf) {
+            Ok(s) => {
+                self.content.push_str(s);
+                Ok(s.len())
+            }
+            Err(_) => Err(io::ErrorKind::WriteZero.into()),
+        }
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        let out = write!(stdout(), "{}", self.content);
+        stdout().flush()?;
+        self.content.clear();
+        out
+    }
+}
 
 struct Output {
     winsize: (usize, usize),
+    editor_contents: EditorContents,
 }
 
 impl Output {
@@ -31,7 +77,10 @@ impl Output {
             .map(|(x,y)| (x as usize, y as usize))
             .unwrap();
 
-        Self { winsize }
+        Self {
+            winsize,
+            editor_contents: EditorContents::new(),
+        }
     }
 
     fn clear_screen() -> crossterm::Result<()> {
@@ -39,18 +88,56 @@ impl Output {
         execute!(stdout(), cursor::MoveTo(0, 0))
     }
 
-    fn rows(&self) {
+    fn rows(&mut self) {
+
         let scr_rows = self.winsize.1;
-        for _ in 0..scr_rows {
-            println!("$\r");
+        let scr_columns = self.winsize.0;
+
+        for i in 0..scr_rows {
+            if i== scr_rows /3 {
+
+                let mut welcome = format!("Orion Editor. Version {}", VERSION);
+                if welcome.len() > scr_columns {
+                    welcome.truncate(scr_columns)
+                }
+
+                let mut padding = (scr_columns - welcome.len()) /2;
+                if padding != 0 {
+                    self.editor_contents.push('$');
+                    padding -= 1
+                }
+
+                (0..padding).for_each(|_| self.editor_contents.push(' '));
+                self.editor_contents.str_push(&welcome);
+
+
+            } else {
+
+            self.editor_contents.push('$');
+            }
+            queue!(
+
+            self.editor_contents,
+                terminal::Clear(ClearType::UntilNewLine)
+            )
+            .unwrap();
+
+            if i < scr_rows - 1 {
+                self.editor_contents.str_push("\r\n");
+            }
         }
     }
 
-    fn refresh_screen(&self) -> crossterm::Result<()> {
-        Self::clear_screen()?;
+    fn refresh_screen(&mut self) -> crossterm::Result<()> {
+
+        queue!(self.editor_contents, terminal::Clear(ClearType::All), cursor:: MoveTo(0,0))?;
 
         self.rows();
-        execute!(stdout(), cursor::MoveTo(0,0))
+
+        queue!(
+            self.editor_contents,
+            cursor::MoveTo(0,0), cursor::Show )?;
+        self.editor_contents.flush()
     }
 }
 
@@ -99,7 +186,7 @@ impl Editor {
         Ok(true)
     }
 
-    fn run(&self) -> crossterm::Result<bool> {
+    fn run(&mut self) -> crossterm::Result<bool> {
         self.output.refresh_screen();
         self.keypress()
     }
@@ -115,7 +202,7 @@ fn  main() -> crossterm::Result<()> {
     terminal::enable_raw_mode()
         .expect("Could not turn on Raw mode");
 
-    let editor = Editor::new();
+    let mut editor = Editor::new();
     while editor.run()
         .expect("Error") {}
 
