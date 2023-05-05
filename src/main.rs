2b45d5bd5dd04cd439b666bc15e7b888817d4f9a
LapaJr31 <jarlat04@gmail.com>
2023-05-05 14:53:26 +0200

    introduced raw mode, cleanup after it, and exiting on crtl-q

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use std::io::Read;
-use crossterm::terminal;
+//use std::io::Read;
+use crossterm::event::{Event, KeyCode, KeyEvent, KeyModifiers};
+use crossterm::{terminal, event};
+//use std::io;
+use std::time::Duration;
 
 struct CleanUp;
 
-
+// Cleaning up for raw mode
 impl Drop for CleanUp {
     fn drop(&mut self) {
         terminal::disable_raw_mode()
@@ -11,6 +14,7 @@ impl Drop for CleanUp {
     }
 }
 
+// reading the inputs
 fn  main() {
 
     let _clean_up = CleanUp;
@@ -18,11 +22,47 @@ fn  main() {
     terminal::enable_raw_mode()
         .expect("Could not turn on Raw mode");
 
-    let mut buf = [0; 1];
-    while std::io::stdin()
-        .read(&mut buf).expect("Failed to read line") == 1 && buf != [b'q'] {}
 
-    panic!();
+    loop {
+        if let Event::Key(event) = event::read()
+            .expect("Failed to read the line") {
+
+                match event {
+                    KeyEvent {
+
+                        code: KeyCode::Char('q'), // exiting on press of q
+                        modifiers: event::KeyModifiers::CONTROL, ..
+
+                    } => break,
+
+                    _ => {
+
+                    //todo
+
+                    }
+            }
+                println!("{:?}\r", event);
+        };
+
+
+
+    }
+
+
+   // let mut buf = [0; 1];
+
+    //while io::stdin()
+    //    .read(&mut buf).expect("Failed to read line") == 1 && buf != [b'q'] {
+
+    //    let charr = buf[0] as char;
+    //    if charr.is_control(){
+    //        println!("{}\r", charr as u8);
+    //    } else {
+    //        println!("{}\r", charr);
+    //    }
+    // }
+
+
 
     //terminal::disable_raw_mode();
 }
